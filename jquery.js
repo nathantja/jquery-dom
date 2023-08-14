95b0cb11838e4ca4c1c3951ec308ce6d427a568c
Nathan Tjahjadi <nathantja@yahoo.com>
2023-08-14 18:05:35 -0500

    finished part one

--- jquery.js
+++ jquery.js
@@ -6,7 +6,6 @@ $("article > img").toggleClass("image-center");
 // 2.
 const $paragraphs = $("article > p");
 $paragraphs[$paragraphs.length - 1].remove();
-console.log($paragraphs)
 
 // 3.
 $("h1").css("fontSize", Math.floor((Math.random() * (100 - 0 + 1))));
@@ -19,9 +18,16 @@ $("li").remove();
 $("ol").append("<p> We're terribly sorry this list's existence. </p>");
 
 // 6.
-let $formControlObject = $(".form-control");
-console.log($formControlObject)
-
 $(".row.mb-5").on("change", ".form-control", function () {
-  $("body").css({ backgroundColor: $(".form-control").val() });
+  const $formControlObject = $(".form-control");
+  const $red = $formControlObject[0].value;
+  const $green = $formControlObject[2].value;
+  const $blue = $formControlObject[1].value;
+
+  $("body").css("backgroundColor", `rgb(${$red}, ${$green}, ${$blue})`);
+});
+
+// 7.
+$("article").on("click", "img", function (){
+  $("article > img").remove();
 });
